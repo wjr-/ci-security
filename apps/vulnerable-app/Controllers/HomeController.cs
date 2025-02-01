59ac6914c82aea53f8d976c6eac64c6e2167611f
Rinorragi <rinorragi@gmail.com>
2025-02-01 10:11:38 +0200

    lab21: actual vulns for the app

--- apps/vulnerable-app/Controllers/HomeController.cs
+++ apps/vulnerable-app/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using vulnerable_app.Models;
 
@@ -6,26 +7,41 @@ namespace vulnerable_app.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+  private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+  public HomeController(ILogger<HomeController> logger)
+  {
+    _logger = logger;
+  }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+  public IActionResult Index()
+  {
+    return View();
+  }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+  public IActionResult Privacy()
+  {
+    return View();
+  }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+  public IActionResult Error()
+  {
+    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+  }
+
+  public IActionResult OpenRedirectVulnerability([FromQuery] string url)
+  {
+    return Redirect(url);
+  }
+
+  public ContentResult XSSVulnerability([FromQuery] string content)
+  {
+    return new ContentResult
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+      StatusCode = (int)HttpStatusCode.OK,
+      ContentType = "text/html",
+      Content = $"<html><body>{content}</body></html>"
+    };
+  }
 }
